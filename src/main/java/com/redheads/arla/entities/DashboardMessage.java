9242179c11d875ed4cd8e91678de14360badd2e9
Mikkel Mouridsen <mikkelmouridsen@protonmail.com>
2021-05-11 12:46:15 +0200

    Update DashboardMessage.java

--- src/main/java/com/redheads/arla/entities/DashboardMessage.java
+++ src/main/java/com/redheads/arla/entities/DashboardMessage.java
@@ -1,12 +1,18 @@
 package com.redheads.arla.entities;
 
+import java.time.LocalDateTime;
+
 public class DashboardMessage extends Entity {
 
     private String msg;
     private MessageType type;
+    private LocalDateTime start;
+    private LocalDateTime end;
 
-    public DashboardMessage(String msg, MessageType type) {
+    public DashboardMessage(String msg, MessageType type, LocalDateTime start, LocalDateTime end) {
         this.msg = msg;
         this.type = type;
+        this.start = start;
+        this.end = end;
     }
 }
